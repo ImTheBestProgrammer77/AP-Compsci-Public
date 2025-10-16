6588ade0acf630e7872415b850ccf272cb2642fe
Avery <amoylan2026@cchsdons.com>
2025-10-16 11:54:32 -0700

    Add files via upload

--- InterestRateCalculator/src/main/java/com/mycompany/interestratecalculator/InterestRateCalculator.java
+++ InterestRateCalculator/src/main/java/com/mycompany/interestratecalculator/InterestRateCalculator.java
@@ -9,6 +9,9 @@ package com.mycompany.interestratecalculator;
  * @author AMoylan2026
  */
 
+import java.util.*;
+import java.text.NumberFormat;
+
 /*
 Write a public main method that uses a different class called InterestRateCalculator
 The main should call for a user to input a financial amount (say $1,000,000)
@@ -27,15 +30,82 @@ Only the variables/methods that should be accessed from outside should be availa
 The InterestRateCalculator should be a "black box". Remember to plan your project, and comment your code throughout.
 */
 
+
+//plan: public method would be set months and set money
+//      calculate intrest use a loop to do for however many months entered
+//      print out to string in the end
+
+
+
+
 public class InterestRateCalculator {
 
     public static void main(String[] args) {
+        
+        //set up variables
+        double dblMoney;
+        int intMonths;
+        //get amounts of money and months
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a dollar amount:");
+        dblMoney = scan.nextDouble();
+        System.out.println("Enter number of payments:");
+        intMonths = scan.nextInt();
+        
+        //set up interest rate calculator and print RATE
+        InterestRateCalc irc = new InterestRateCalc();
+        System.out.println(irc);
+        
+        //set money and months and calculate interest
+        irc.setMoney(dblMoney);
+        irc.setMonths(intMonths);
+        irc.calculateInterest();
         
     }
 }
 
 class InterestRateCalc {
     
-    
-    
+    //set up variables in InterestRateClass
+  public double RATE = 0.05;
+  private double money;
+  private int months;
+  private double amountM;
+  private double amountR;
+  private String strAmount;
+  
+  //public method set money
+  public void setMoney(double inM){
+      money = inM;
+}
+  //public method set months
+  public void setMonths (int inMt){
+      months = inMt;
+  }
+  
+  //public method calculate
+  public void calculateInterest (){
+      
+    //loop for months amount of times
+  for ( int i = 1; i <= months; i++) {
+     System.out.println("Monthly payment " + i); 
+     
+     //math and print out
+     amountM =(money/months);
+      amountR = amountM + amountM * RATE;
+      NumberFormat formatDollars = NumberFormat.getCurrencyInstance(Locale.US);
+      strAmount = formatDollars.format(amountR);
+      System.out.println(strAmount);
+      money = money - amountM;
+      months--;
+      
+  }
+  }
+  
+  //public method to String to print out RATE
+  public String toString(){
+            String myString = "My interest rate is " + RATE;
+                   
+            return myString;
+  }
 }
